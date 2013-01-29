1db9bf3b85f1a861ecb093a790395a0bf7916ad4
Hegz <adam.fairbrother@gmail.com>
2013-01-28 23:18:30 -0800

    Trithemius cipher added
    Trithemius cipher has been added, as well as an adjustment to the
    options of Tabula_recta.pl. -t now enables Trithemius mode, rather
    then dumping the table.  -r now dumps the table.

--- Tabula_recta.pl
+++ Tabula_recta.pl
@@ -11,11 +11,12 @@
 #               -s <string>       String to encode.
 #               -p                Preserve whitespaces and punctuation.
 #               -d                Decode the cypher, otherwise encode
-#               -t                Dump the Tabula Recta as plain text
+#               -t                Enable Trithemius Mode, -k ignored.
 #               -a                Autokey mode
+#               -r                Print the Tabula Recta used as plain text
 #
 #       AUTHOR: Hegz
-#      VERSION: 1.0
+#      VERSION: 2.0
 #      CREATED: 28/01/13 13:20:16
 #===============================================================================
 
@@ -26,6 +27,7 @@ use Getopt::Std;
 my %opts;
 getopts('s:k:pdta', \%opts);
 
+# Table options can be adjusted here to include numbers, any characters.
 my @alphabet = 'a' .. 'z';
 
 # Build hash tables for faster encode/decode
@@ -36,22 +38,27 @@ for (0 .. (@alphabet - 1)) {
 	$seq2alpha{$_} = $alphabet[$_];
 }
 
-if ($opts{'t'}) {
+if ($opts{'r'}) {
 	#print the table, and exit
 	show_table();
 	exit 0;
 }
 
 unless ($opts{'p'}) {
-#Strip all punctuation and whitespace from the encode string
+	#Strip all punctuation and whitespace from the encode string
 	$opts{'s'} =~ s/\s*//g;
 	$opts{'s'} =~ s/[!?\/\\<>.,'"@()\[\]\-;]*//g;
 }
 
-my @message = split(//, $opts{'s'});
-my @key = split(//,$opts{'k'});
-
-
+my @message = split(//, lc($opts{'s'}));
+my @key;
+if ($opts{'t'}) {
+	# Trithemius mode, The key is the same as the alphabet.
+@key = @alphabet;
+}
+else {
+	@key = split(//,lc($opts{'k'}));
+}
 
 my @out;
 unless ($opts{'d'}) {
@@ -102,7 +109,7 @@ print "\n";
 exit 0;
 
 sub show_table {
-# Spit out a pretty Tabula based on our language options.
+# Spit out a Tabula based on our language options.
 	my $hsep = " ";
 		print "  ";
 		for (@alphabet){
